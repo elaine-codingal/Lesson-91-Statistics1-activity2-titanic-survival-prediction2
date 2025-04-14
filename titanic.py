016df33f4ed30a5a123fabc90fd2d1f2f23b85b2
elaine <elainemuhombe.codingal@gmail.com>
2025-04-14 16:51:07 +0300

    chore: Modify code

--- titanic.py
+++ titanic.py
@@ -6,27 +6,17 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 #Load the dataset
-data=pd.read_csv('Titanic Dataset.csv')
+data=pd.read_csv('Titanic_Dataset.csv')
 
-print(data.head())
+print(data.head(5))
 
-#Median value of age and fare
-median_age=np.median(data['Age'])
-print("Median value of age-", median_age)
+#Mean value of age
+mean_age=np.mean(data['Age'])
+print("Mean Age is - ", mean_age)
 
-median_fare=np.median(data['Fare'])
-print("Median value of fare -", median_fare)
-
-"""###**Mode value of Age and Pclass**"""
-mode_age=stats.mode(data['Age'])
-print("Mode value of age -", mode_age)
-
-mode_class=stats.mode(data['Pclass'])
-print("Mode value of class -", mode_class)
-
-"""###**Mode value of Categorical Feature - Gender**"""
-mode_gender=data['Gender'].value_counts().index[0]
-print("Mode of Feature Gender -", mode_gender)
+#Mean value of fare
+mean_fare=np.mean(data['Fare'])
+print("Mean Fare is - ",mean_fare)
 
 
 
